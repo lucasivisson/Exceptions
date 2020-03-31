6191b613e4d609affc404cf7223569f9d9e019d6
Ives Moreira <53413719+Ives-Gomes@users.noreply.github.com>
2020-03-31 15:05:36 -0300

    Indentação

--- src/jogo/Main.java
+++ src/jogo/Main.java
@@ -15,12 +15,12 @@ public class Main {
         System.out.println("Você é o Robo Vermelho.");
         
         do {
-        System.out.println("Determine a posição do alimento que o Robo Vermelho irá encontrar: ");
-        System.out.println("OBS: Posições no limite de 1 a 5.");
-        System.out.println("Digite o X do alimento: ");
-        xAlimento = teclado.nextInt();
-        System.out.println("Digite o Y do alimento: ");
-        yAlimento = teclado.nextInt();
+            System.out.println("Determine a posição do alimento que o Robo Vermelho irá encontrar: ");
+            System.out.println("OBS: Posições no limite de 1 a 5.");
+            System.out.println("Digite o X do alimento: ");
+            xAlimento = teclado.nextInt();
+            System.out.println("Digite o Y do alimento: ");
+            yAlimento = teclado.nextInt();
         } while ((xAlimento > 5 || xAlimento < 1) || (yAlimento > 5 || yAlimento < 1));
         
         System.out.println("\n");
@@ -30,18 +30,18 @@ public class Main {
         do {
             //i = coluna
             //j = linha
-            for(int i=0; i<5; i++){
+            for(int i = 0; i < 5; i++){
                 System.out.println("\n");
-                for(int j=0; j<5; j++){
-                        if(i == (yAlimento - 1) && j == (xAlimento - 1)){
-                            System.out.print("A ");
-                        }else if(robo.GetX() == j && robo.GetY() == i){
-                            System.out.print("R ");
-                        }else {
-                            System.out.print(". ");
-                        }
+                for(int j = 0; j < 5; j++){
+                    if(i == (yAlimento - 1) && j == (xAlimento - 1)){
+                        System.out.print("A ");
+                    } else if(robo.GetX() == j && robo.GetY() == i){
+                        System.out.print("R ");
+                    } else {
+                        System.out.print(". ");
                     }
                 }
+            }
             
             if(robo.encontrouAlimento((xAlimento - 1), (yAlimento- 1))) {
             	jaEncontrou = true;
